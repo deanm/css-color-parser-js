3224bdef3a6e07168bf10ec68443dc93e93cc5c0
Dean McNamee <dean@gmail.com>
2012-12-07 14:44:14 +0100

    Add links in the README and source to the github repository.

--- csscolorparser.js
+++ csscolorparser.js
@@ -1,5 +1,7 @@
 // (c) Dean McNamee <dean@gmail.com>, 2012.
 //
+// https://github.com/deanm/css-color-parser-js
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to
 // deal in the Software without restriction, including without limitation the
